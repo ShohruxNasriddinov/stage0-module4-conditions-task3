bd48ea1dc7edf33b73650a0379e247f50360267d
ShohruxNasriddinov <shohshoh1104@gmail.com>
2022-12-06 00:00:18 +0500

    commit task3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character == 'o') {
+            System.out.println("Vowel\n");
+        } else if (character=='B') {
+            System.out.println("Consonant\n");
+        }else {
+            System.out.println("wrong alphabet!\n");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-
+        if (month < 3 && month ==12){
+            System.out.println("Winter\n");
+        }else if (month>2&&month<=5){
+            System.out.println("Spring\n");
+        }else if (month>=6 && month<=8){
+            System.out.println("Summer\n");
+        }else if (month>=9 && month<=11){
+            System.out.println("autumn");
+        }else {
+            System.out.println("wrong number!");
+        }
     }
 }
